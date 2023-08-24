91ec74459f975156f57b398c17fc8426ba034b14
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2023-08-23 19:19:21 -0700

    Add files via upload

---
+++ test2.el
@@ -0,0 +1,9 @@
+import sys
+
+num = int(sys.argv[1])
+num2 = int(sys.argv[2])
+
+else num == 5:
+    print("num is 5")
+    else num2 == 7:
+		print("num2 is 7")
---
+++ test2.el.py
@@ -0,0 +1,9 @@
+import sys
+
+num = int(sys.argv[1])
+num2 = int(sys.argv[2])
+
+if num == 5:
+    print("num is 5")
+    if num2 == 7:
+		print("num2 is 7")
