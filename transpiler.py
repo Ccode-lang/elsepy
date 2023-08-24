4d3150ece5c530ab5f57c1d023ffe4708fb4a803
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2023-08-23 19:18:35 -0700

    Update transpiler.py

--- transpiler.py
+++ transpiler.py
@@ -9,9 +9,17 @@ output = open(sys.argv[1] + ".py", "a")
 
 inelse = False
 
+def lengthofws(string):
+	return len(string)-len(string.lstrip())
+
+
+lastlength = 0
+
 for line in elsepycode:
 	line = line.rstrip()
 	if line.strip().startswith("else"):
+		if lengthofws(line) > lastlength:
+			inelse = False
 		if line.strip().startswith("else:"):
 			inelse = False
 			output.write(line + "\n")
@@ -21,6 +29,7 @@ for line in elsepycode:
 				inelse = True
 			else:
 				output.write(line.replace("else", "elif") + "\n")
+		lastlength = lengthofws(line)
 			
 	else:
 		output.write(line + "\n")
